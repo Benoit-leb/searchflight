38a7342ea3e3b290dcc4ac90a28f64042eddd619
Benoît Lebrun <benoit@benoits-macbook-pro.home>
2021-06-07 15:57:42 +0200

    search flight

--- src/layout/Page/StyledPage.js
+++ src/layout/Page/StyledPage.js
@@ -5,6 +5,10 @@ const StyledPage = styled.div`
 	max-width: ${$.maxWidth};
 	min-height: 100vh;
 	margin: 0 1rem 3rem;
+	@media (min-width: ${$.sm}) {
+		margin: 0 auto 3rem;
+		padding: 0 1rem;
+	}
 `;
 
 export default StyledPage;
