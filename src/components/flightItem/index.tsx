511e6a72e60bb78b30806d814d1f012048f59fb5
Benoît Lebrun <benoit@benoits-macbook-pro.home>
2021-06-07 10:32:16 +0200

    search flight

--- src/components/flightItem/index.tsx
+++ src/components/flightItem/index.tsx
@@ -7,22 +7,26 @@ import Button from "@components/button/button";
 const FlightItem: FC<{ item: Iflight, included: Iincluded }> = (props) => {
 
   const { item } = props;
+  const { included } = props;
   return (
     <StyledflightItem>
       <div className="row around-xs top-xs">
-        <div className="col-xs-2 col-sm-2">{item.flightNumber}</div>
-        <div className="col-xs-6 col-sm-4">
+        <div className="col-xs-12 col-sm-2">
+          <div>{included[item.airline].name}</div>
+          <div>{item.flightNumber}</div>
+        </div>
+        <div className="col-xs-8 col-sm-4">
           <div className="box">
             <span>{item.takeoff}</span>
             <span> - </span>
             <span>{item.landing} (local time)</span>
           </div>
           <div className="box">
-            <div>{`${props.included[item.departureAirport].city} - ${props.included[item.departureAirport].name}`}</div>
-            <div>{`${props.included[item.arrivalAirport].city} - ${props.included[item.arrivalAirport].name}`}</div>
+            <div>{`${included[item.departureAirport].city} - ${included[item.departureAirport].name}`}</div>
+            <div>{`${included[item.arrivalAirport].city} - ${included[item.arrivalAirport].name}`}</div>
           </div>
         </div>
-        <div className="col-xs-12 col-sm-3 last-xs center-xs start-sm">{parseInt(item.duration / 60)}h {item.duration % 60}min</div>
+        <div className="col-xs-12 col-sm-3 last-xs start-xs">{parseInt(item.duration / 60)}h {item.duration % 60}min</div>
         <div className="col-xs-4 col-sm-3 last-sm">
           <div className="box">
             <div className="row between-sm center-xs">
